d1c689bd796972f9cb805a2e278fa496e6779b87
Johan Vlassak <johan.vlassak@gmail.com>
2021-12-31 14:37:28 -0500

    added soup object, figuring out the html now

--- webscraper.py
+++ webscraper.py
@@ -1,5 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
+
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
 
-print(page.text)
+print(results.prettify())
